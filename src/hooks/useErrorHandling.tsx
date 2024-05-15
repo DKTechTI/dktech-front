7f05efe0ae8947f590d43cd9d2fcc18722603a14
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-05-15 13:11:58 -0300

    refact: hook of errors

--- src/hooks/useErrorHandling.tsx
+++ src/hooks/useErrorHandling.tsx
@@ -1,14 +1,26 @@
+import { AxiosError, isAxiosError } from 'axios'
+import toast from 'react-hot-toast'
+
 interface HandleErrorResponseProps {
-  error: number
-  message: string
-  referenceError: { [errorStatus: number]: { [keyMessage: string]: string } }
+  error: Error | AxiosError
+  errorReference: { [errorStatus: number]: { [keyMessage: string]: string } }
+  defaultErrorMessage: string
 }
 
 const useErrorHandling = () => {
-  const handleErrorResponse = ({ error, message, referenceError }: HandleErrorResponseProps) => {
-    if (referenceError && referenceError[error] && referenceError[error][message]) return referenceError[error][message]
+  const handleErrorResponse = ({ error, errorReference, defaultErrorMessage }: HandleErrorResponseProps) => {
+    if (!isAxiosError(error)) return toast.error(defaultErrorMessage)
 
-    return null
+    if (error.response) {
+      if (
+        errorReference &&
+        errorReference[error.response.status] &&
+        errorReference[error.response.status][error.response.data.message]
+      )
+        return toast.error(errorReference[error.response.status][error.response.data.message])
+    }
+
+    return toast.error(defaultErrorMessage)
   }
 
   return { handleErrorResponse }
