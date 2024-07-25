3b3f4168eaabeed384c9854e1039d93ec177c73c
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-07-25 18:19:02 -0300

    refactor: Remove hook do not use.

--- src/views/pages/projetos/configurar-projeto/DeviceConfig/index.tsx
+++ src/views/pages/projetos/configurar-projeto/DeviceConfig/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import { Box, CircularProgress, Typography } from '@mui/material'
